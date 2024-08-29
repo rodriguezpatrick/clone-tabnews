1c9aae45e58a323a82cd8bb73196b567a2acfe86
rodriguezpatrick <150753384+rodriguezpatrick@users.noreply.github.com>
2024-08-29 16:19:14 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Que você se acostume com notícias boas, porque muitas coisas vão dar certo para você!</h1>
+function Home() {
+  return (
+    <h1>
+      Que você se acostume com notícias boas, porque muitas coisas vão dar certo
+      para você!
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
