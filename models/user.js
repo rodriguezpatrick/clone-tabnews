99cab1e8f147d2adde802e7995b830ea44249ef9
Patrick <patrickrodrigues740@gmail.com>
2025-09-04 07:39:23 -0300

    feat: create `user` model and `api/v1/users` endpoint

---
+++ models/user.js
@@ -0,0 +1,72 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors";
+
+async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function runInsertQuery(userInputValues) {
+    const results = await database.query({
+      text: `
+            INSERT INTO 
+              users (username, email, password) 
+            VALUES 
+              ($1, $2, $3)
+            RETURNING
+              *
+                ;`,
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
+    return results.rows[0];
+  }
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+            SELECT 
+              email 
+            FROM
+              users 
+            WHERE
+              LOWER(email) = LOWER($1) 
+            ;`,
+      values: [email],
+    });
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está cadastrado.",
+        action: "Utilize outro email para realizar o cadastro",
+      });
+    }
+  }
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+            SELECT 
+              username 
+            FROM
+              users 
+            WHERE
+              LOWER(username) = LOWER($1) 
+            ;`,
+      values: [username],
+    });
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O nome de usuário informado já está cadastrado.",
+        action: "Utilize outro nome de usuário para realizar o cadastro",
+      });
+    }
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
