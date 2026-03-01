57f6441f601f5e3816395167fff4dd465281130e
rodriguezpatrick <150753384+rodriguezpatrick@users.noreply.github.com>
2026-03-01 00:38:44 +0000

    feat: allow `update:user:others` to update other users

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -97,6 +97,11 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function addFeaturesToUser(userObject, features) {
+  const updatedUser = await user.addFeatures(userObject.id, features);
+  return updatedUser;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -107,6 +112,7 @@ const orchestrator = {
   getLastEmail,
   extractUUID,
   activateUser,
+  addFeaturesToUser,
 };
 
 export default orchestrator;
